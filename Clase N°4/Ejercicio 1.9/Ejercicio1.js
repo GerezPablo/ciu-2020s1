203c6d804ecb3b51b6ed7e765ff39ee89e98053d
Pablo Gerez <pablogerez2000@gmail.com>
2020-05-10 15:21:21 -0300

    Reordenamiento

---
+++ Clase N°4/Ejercicio 1.9/Ejercicio1.js
@@ -0,0 +1,24 @@
+//Variables
+var a = 10;
+var b = 15;
+var c = 20;
+
+var d = a+b+c;
+
+a = b;
+b = 10;
+
+//Mensajes con Variables
+var nombre = "Carpincho";
+var edad = "28";
+var cumple = "09/05/92";
+var ciudad = "Espeleta"; 
+var ocupacion = "Lolero de vocacion";
+
+console.log(`
+    Nombre: ${nombre} \n
+    Edad: ${edad}  \n
+    Fecha de nacimiento: ${cumple} \n
+    Ciudad: ${ciudad} \n
+    Ocupacion: ${ocupacion}
+`)
